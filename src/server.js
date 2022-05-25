bec5bd41c6f3e7f2c30fc382f7d23b876dc80074
Jacob0312 <jaenk0312@gmail.com>
2022-05-25 09:37:12 +0200

    routers

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
-
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-	console.log(`${req.url} ${req.method}`);
-	next();
-};
-
-const handleHome = (req, res) => {
-	return res.send(`I love middlewares`);
-};
-app.get("/", logger, handleHome);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 function handleListening() {
 	console.log(`Server listening on port http://localhost:${PORT}`);
