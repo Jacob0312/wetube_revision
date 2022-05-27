072923a3b03bfa45a41033c8c0cf74d51f21d070
Jacob0312 <jaenk0312@gmail.com>
2022-05-27 11:06:42 +0200

    Video validation (Try / Catch errors)
    Setting default values for db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,19 +26,21 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
 	const { title, description, hashtags } = req.body;
-	await Video.create({
-		title,
-		description,
-		createdAt: Date.now(),
-		hashtags: hashtags
-			.split(",")
-			.map((word) =>
-				!word.trim().startsWith("#") ? `#${word.trim()}` : word.trim()
-			),
-		meta: {
-			views: 0,
-			rating: 0,
-		},
-	});
-	res.redirect("/");
+	try {
+		await Video.create({
+			title,
+			description,
+			hashtags: hashtags
+				.split(",")
+				.map((word) =>
+					!word.trim().startsWith("#") ? `#${word.trim()}` : word.trim()
+				),
+		});
+		res.redirect("/");
+	} catch (error) {
+		return res.render("upload", {
+			pageTitle: "Upload Video",
+			errorMessage: error._message,
+		});
+	}
 };
