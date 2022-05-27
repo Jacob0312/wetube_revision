7bb0d45b9d20b799c9a72a7970c18b85e25d6174
Jacob0312 <jaenk0312@gmail.com>
2022-05-27 10:51:23 +0200

    Second way of uploading on mongodb

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,7 +26,7 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
 	const { title, description, hashtags } = req.body;
-	const video = new Video({
+	await Video.create({
 		title,
 		description,
 		createdAt: Date.now(),
@@ -40,7 +40,5 @@ export const postUpload = async (req, res) => {
 			rating: 0,
 		},
 	});
-	const createVid = await video.save();
-	console.log(createVid);
 	res.redirect("/");
 };
