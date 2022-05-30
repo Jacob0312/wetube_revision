8c74d0f4b9c86b060881927a3e29d2a438dbe1ca
Jacob0312 <jaenk0312@gmail.com>
2022-05-30 08:16:40 +0200

    using schema mongoose
    as well as making hashtags visible on watch screen

--- src/models/Video.js
+++ src/models/Video.js
@@ -10,12 +10,10 @@ const videoSchema = new mongoose.Schema({
 		rating: { type: Number, required: true, default: 0 },
 	},
 });
-
-videoSchema.pre("save", async function () {
-	this.hashtags = this.hashtags[0]
+videoSchema.static("formatHashtags", function (hashtags) {
+	return hashtags
 		.split(",")
 		.map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
-
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
